8159ee6186fb3779a2dddc889a2170a418df931d
[200~Kaiweinreiss~ <kaiwein@web.de>
2023-05-18 18:03:34 +0200

    add code to compute the activity sequnces and also the time data and also make preprocesing so that filtered logs are possible

--- code/functions/preprossesing.py
+++ code/functions/preprossesing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 ## preparing the cel
-def prepare_ocel(fn):
+def prepare_ocel(fn, filter = None):
     ocel = pd.read_csv('../sourcedata/%s.csv' % fn)
     ocel['ocel:timestamp']=  pd.to_datetime(ocel['ocel:timestamp'])
     col_transform = {"ocel:eid": "Event_ID", "ocel:timestamp": "Timestamp", "ocel:activity": "Activity", "ocel:type:items": "Items",
@@ -15,7 +15,8 @@ def prepare_ocel(fn):
     ocel['Packages']=  ocel['Packages'].fillna('')
     ocel['Packages']=  ocel['Packages'].apply(lambda x: x.strip('][').split(', ')).apply(lambda x: [t.replace("'","") for t in x]).apply(lambda x: x[-1])
     ocel['Customers']=  ocel['Customers'].apply(lambda x: x[-1])
-
+    if filter is not None:
+        ocel = ocel[ocel['Activity'].isin(filter)]
 
     # Perform label encoding with letters and create a dictionary
     act_uni = ocel['Activity'].unique()
@@ -45,16 +46,18 @@ def flatten(OCEL, on, printen = False):
 
 #generating the csv files for the imput
 
-def gen_flatted_comp_csv(OCEL,flattedby, drops_col, printen = False):
-
-    OCEL = OCEL.rename(columns={flattedby:'Case_ID'})
+def gen_enriched_single_plus_csv(OCEL,flatted_by, drops_col,csvname, printen = False):
+    OCEL = OCEL.rename(columns={flatted_by:'Case_ID'})
     enriched_log = OCEL.set_index('Case_ID').reset_index().drop(columns=drops_col).sort_values(['Case_ID','Timestamp'])
-    enriched_log['amount_of_items'] = [len(t) for t in enriched_log['Items']]
-    enriched_log.to_csv('../data/orders_complete_enriched.csv')
+    enriched_log['Amount_Items'] = [len(t) for t in enriched_log['Items']]
+    enriched_log.to_csv(f'../data/{csvname}_enriched.csv')
     single_log = OCEL.set_index('Case_ID').reset_index()[['Case_ID','Activity','Timestamp']].sort_values(['Case_ID','Timestamp'])
-    single_log.to_csv('../data/orders_complete_single.csv')
+    single_log.to_csv(f'../data/{csvname}_single.csv')
     if printen:
         pd.display(enriched_log[120:140])
         pd.display(single_log[120:140])
     return enriched_log, single_log
-    
+
+act_for_filtered_order = ['place order','confirm order','payment reminder','pay order']
+act_for_filtered_item = ['place order','confirm order', 'item out of stock', 'reorder item', 'pick item' , 'create package', 'send package', 'failed delivery', 'package delivered']
+act_for_filtered_pack = ['create package', 'send package', 'failed delivery', 'package delivered']
